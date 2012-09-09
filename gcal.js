9f3418916586e5bf43089a5c358ac6265d6d4636
smilekzs <smilekzs@gmail.com>
2012-09-09 12:41:38 +0800

    API now works.

--- gcal.js
+++ gcal.js
@@ -1,4 +1,8 @@
 //Google API authorization handling
+var GAPI_KEY="AIzaSyAKhKf_NyHlqxw6BdT67J_3gbGa9oSfb4A";
+var GAPI_CLIENT_ID="584858632131.apps.googleusercontent.com";
+var GAPI_CLIENT_SECRET="_Ra9CVJCbrTxg8Sy1BaDFkNV";
+
 var GAPI_REQUEST_URL='https://www.google.com/accounts/OAuthGetRequestToken';
 var GAPI_AUTHORIZE_URL='https://www.google.com/accounts/OAuthAuthorizeToken';
 var GAPI_ACCESS_URL='https://www.google.com/accounts/OAuthGetAccessToken';
@@ -9,15 +13,22 @@ var oauth = ChromeExOAuth.initBackgroundPage({
   'request_url': GAPI_REQUEST_URL,
   'authorize_url': GAPI_AUTHORIZE_URL,
   'access_url': GAPI_ACCESS_URL,
-  'consumer_key': 'anonymous',
-  'consumer_secret': 'anonymous',
+  'consumer_key': GAPI_CLIENT_ID,
+  'consumer_secret': GAPI_CLIENT_SECRET,
   'scope': GAPI_SCOPE,
   'app_name': 'THU Calendar 2 Google'
 });
 
-oauth.authorize(function(){
-    alert('yes');
-});
+function print_result(a){
+    console.log(a);
+}
+function authorized(){
+    oauth.sendSignedRequest(
+        'https://www.googleapis.com/calendar/v3/users/me/calendarList',
+        print_result
+    );
+}
+oauth.authorize(authorized);
 
 function createCalendar(name){
     /*
